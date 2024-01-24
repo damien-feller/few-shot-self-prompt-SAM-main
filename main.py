4ca7f0407ba6f4cf45a7e04807a46388be62c5cc
damien-feller <148772466+damien-feller@users.noreply.github.com>
2024-01-24 10:55:43 +0000

    added image augmentation transforms

--- main.py
+++ main.py
@@ -272,7 +272,7 @@ def train(args, predictor):
     i = 0
 
     # image augmentation and embedding processing
-    num_augmentations = 3  # Number of augmented versions to create per image
+    num_augmentations = 10  # Number of augmented versions to create per image
 
     for fname in tqdm(fnames):
         # Read data
