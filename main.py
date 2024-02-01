12206f58dd7c1de2013e5482383fb78d8924596e
damien-feller <148772466+damien-feller@users.noreply.github.com>
2024-02-01 14:45:37 +0000

    added 3x3 kernal, 3 erosions, 5 dilations to visualisation

--- main.py
+++ main.py
@@ -127,6 +127,12 @@ def visualize_predictions(images, masks, model, num_samples=3, val=False):
         # Reshape the prediction to the original mask shape
         pred = pred_flat.reshape(mask.shape)
 
+        # Define the kernel for dilation
+        kernel = np.ones((3, 3), np.uint8)
+
+        pred = cv2.dilate(pred, kernel, iterations=5)
+        pred = cv2.erode(pred, kernel, iterations=3)
+
         plt.figure(figsize=(6, 4))
         plt.subplot(1, 2, 1)
         plt.imshow(mask, cmap='gray')
@@ -220,7 +226,7 @@ def train(args, predictor):
     predicted_masks_svm = predict_and_reshape(svm_model, val_embeddings_flat, (len(val_embeddings_tensor), 64, 64))
 
     # Define the kernel for dilation
-    kernel = np.ones((10, 10), np.uint8)
+    kernel = np.ones((3, 3), np.uint8)
 
     predicted_masks_svm = cv2.dilate(predicted_masks_svm, kernel, iterations=5)
     predicted_masks_svm = cv2.erode(predicted_masks_svm, kernel, iterations=3)
