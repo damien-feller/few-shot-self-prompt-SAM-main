dedbdf9d5775f55f3217145535c4dc20484542c4
damien-feller <148772466+damien-feller@users.noreply.github.com>
2024-02-29 12:23:16 +0000

    combine edge detection

--- main.py
+++ main.py
@@ -169,11 +169,11 @@ def visualize_predictions(org_img, images, masks, model, num_samples=3, val=Fals
         edges_img = cv2.normalize(edges_img, None, 0, 255, cv2.NORM_MINMAX)
 
         # Combine heatmap with edge
-        combo_heat = heatmap_normalized + (1*edges_heat)
+        combo_heat = heatmap_normalized + (2*edges_heat)
         combo_heat = cv2.normalize(combo_heat, None, 0, 255, cv2.NORM_MINMAX).astype('uint8')
-        combo_gaussian = gaussian_filtered + (1 * edges_gaussian)
+        combo_gaussian = gaussian_filtered + (2 * edges_gaussian)
         combo_gaussian = cv2.normalize(combo_gaussian, None, 0, 255, cv2.NORM_MINMAX).astype('uint8')
-        combo_median = median_filtered + (1 * edges_median)
+        combo_median = median_filtered + (2 * edges_median)
         combo_median = cv2.normalize(combo_median, None, 0, 255, cv2.NORM_MINMAX).astype('uint8')
 
         # Apply threshold to the original and filtered heatmaps
