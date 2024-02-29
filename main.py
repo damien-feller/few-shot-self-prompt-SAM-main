64e4b9ce47066b9cabe84ab096287e9032328fa8
damien-feller <148772466+damien-feller@users.noreply.github.com>
2024-02-29 15:06:02 +0000

    added SAM segmentation

--- main.py
+++ main.py
@@ -536,7 +536,8 @@ def train(args, predictor):
         SAM_pred = []
         for j in range(len(val_images)):
             masks_pred, logits = SAM_predict(predictor, val_images[j] , bounding_box=BBoxes_Otsu[j], point_prompt=None)
-            SAM_pred.append(masks_pred[0])
+            mask_SAM = cv2.resize(masks_pred[0], dsize=(64, 64), interpolation=cv2.INTER_NEAREST)
+            SAM_pred.append(mask_SAM)
 
 
 
