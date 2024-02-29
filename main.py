7484177c290d156487554e49446bf2366d0399a6
damien-feller <148772466+damien-feller@users.noreply.github.com>
2024-02-29 13:35:48 +0000

    added otsu to prediciton heatmap

--- main.py
+++ main.py
@@ -452,7 +452,7 @@ def train(args, predictor):
         prediction_time_otsu = (end_time - start_time) / 25
         otsu_original = predicted_masks_otsu
 
-        print(otsu_original.shape)
+        print(np.array(otsu_original).shape)
 
         # Define the kernel for dilation
         kernel = np.ones((2, 2), np.uint8)
