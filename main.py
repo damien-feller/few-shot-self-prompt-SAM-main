53f4545646174e215d3a93db77861d866951b7ac
damien-feller <148772466+damien-feller@users.noreply.github.com>
2024-03-06 12:59:20 +0000

    include point prompt

--- main.py
+++ main.py
@@ -963,7 +963,7 @@ def main():
     random.seed(42)
     
     # register the SAM model
-    sam = sam_model_registry[args.model_type](checkpoint=args.checkpoint).to(args.device)
+    sam = sam_model_registry['vit_b'](checkpoint=args.checkpoint).to(args.device)
     global predictor
     predictor = SammedPredictor(sam)
     print('SAM model loaded!', '\n')
