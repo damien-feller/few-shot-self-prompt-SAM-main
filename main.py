69a46120f5f3d25cad1cb1f9d35c786c8f08c76d
damien-feller <148772466+damien-feller@users.noreply.github.com>
2024-03-07 11:01:00 +0000

    intial

--- main.py
+++ main.py
@@ -565,11 +565,11 @@ def train(args, predictor):
             param_dist = {
                 'learning_rate': uniform(0.01, 0.3),  # Learning rate
                 'n_estimators': randint(100, 1000),  # Number of trees
-                'max_depth': randint(3, 10),  # Maximum depth of trees
-                'min_child_weight': randint(1, 10),  # Minimum sum of instance weight(hessian) needed in a child
-                'colsample_bytree': uniform(0.3, 0.7),  # Adjusted to ensure max value is <= 1
-                'subsample': uniform(0.6, 0.4),  # Adjusted to ensure max value is <= 1
-                'gamma': uniform(0, 5),  # Minimum loss reduction required to make a further partition on a leaf node
+                #'max_depth': randint(3, 10),  # Maximum depth of trees
+                #'min_child_weight': randint(1, 10),  # Minimum sum of instance weight(hessian) needed in a child
+                #'colsample_bytree': uniform(0.3, 0.7),  # Adjusted to ensure max value is <= 1
+                #'subsample': uniform(0.6, 0.4),  # Adjusted to ensure max value is <= 1
+                #'gamma': uniform(0, 5),  # Minimum loss reduction required to make a further partition on a leaf node
                 'reg_alpha': uniform(0.0, 1.0),  # L1 regularization term on weights
                 'reg_lambda': uniform(0.0, 1.0)  # L2 regularization term on weights
             }
