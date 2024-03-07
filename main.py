559d95d677b5dd4af555413921c546c526ba71e0
damien-feller <148772466+damien-feller@users.noreply.github.com>
2024-03-07 10:54:33 +0000

    intial

--- main.py
+++ main.py
@@ -549,7 +549,7 @@ def train(args, predictor):
         train_embeddings_oversampled, train_labels_oversampled = ros.fit_resample(train_embeddings_flat, train_labels_flat)
 
         # Initialize the XGBoost classifier model
-        model = xgb.XGBClassifier(objective='binary:logistic', colsample_bytree=0.54, gamma = 0.46, learning_rate=0.0383, subsample = 0.8,
+        model = xgb.XGBClassifier(objective='binary:logistic', colsample_bytree=0.54, gamma = 0.46, learning_rate=0.0383, subsample = 0.8, booster = 'gblinear',
                                   max_depth=8, min_child_weight = 4, alpha=10, n_estimators=550, reg_alpha = 0.474, reg_lambda = 0.098, verbosity=2, device = "cuda")
         model.fit(train_embeddings_flat, train_labels_flat)
 
