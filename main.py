a59b76999e2a49d7cd6e37fe5c0adc92bc766c31
damien-feller <148772466+damien-feller@users.noreply.github.com>
2024-03-20 15:59:49 +0000

    uniform background points

--- main.py
+++ main.py
@@ -287,6 +287,7 @@ def SAM_predict(predictor, image=None, bounding_box=None, point_prompt=None, hea
     # Optionally, you can combine the filled holes with the original mask to ensure only the holes are filled
     final_result = cv2.bitwise_or(mask_SAM, holes_filled)
     _, final_result = cv2.threshold(final_result, 127, 1, cv2.THRESH_BINARY)
+    print(final_result.shape)
 
 
     return final_result, logits
