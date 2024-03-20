4d34b1e204c40a885cae4ec66fe9f1697804b45c
damien-feller <148772466+damien-feller@users.noreply.github.com>
2024-03-20 13:39:36 +0000

    uniform background points

--- segment_anything/modeling/transformer.py
+++ segment_anything/modeling/transformer.py
@@ -160,7 +160,7 @@ class ModifiedTwoWayTransformer(nn.Module):
                     )
                 )
 
-        self.final_attn_token_to_image = ModifiedAttention(
+        self.final_attn_token_to_image = ModifiedAttentionFinal(
             embedding_dim, num_heads, attention_downsample_rate, apply_modification=True
         )
         self.norm_final_attn = nn.LayerNorm(embedding_dim)
@@ -499,8 +499,84 @@ class ModifiedAttention(nn.Module):
             # Flatten the confidence_map to [4096]
             confidence_map_flat = confidence_map.view(-1)
             confidence_map_flat = confidence_map_flat.unsqueeze(0).unsqueeze(1).unsqueeze(-1)  # Add batch dimension
-            print(attn.shape)
-            print(confidence_map_flat.shape)
+            attn = attn * confidence_map_flat
+
+        attn = torch.softmax(attn, dim=-1)
+
+        # Get output
+        out = attn @ v
+        out = self._recombine_heads(out)
+        out = self.out_proj(out)
+
+        return out
+
+class ModifiedAttentionFinal(nn.Module):
+    """
+    An attention layer that allows for downscaling the size of the embedding
+    after projection to queries, keys, and values.
+    """
+
+    def __init__(
+        self,
+        embedding_dim: int,
+        num_heads: int,
+        downsample_rate: int = 1,
+        apply_modification: bool = False
+    ) -> None:
+        super().__init__()
+        self.embedding_dim = embedding_dim
+        self.internal_dim = embedding_dim // downsample_rate
+        self.num_heads = num_heads
+        self.apply_modification = apply_modification
+        assert self.internal_dim % num_heads == 0, "num_heads must divide embedding_dim."
+
+        self.q_proj = nn.Linear(embedding_dim, self.internal_dim)
+        self.k_proj = nn.Linear(embedding_dim, self.internal_dim)
+        self.v_proj = nn.Linear(embedding_dim, self.internal_dim)
+        self.out_proj = nn.Linear(self.internal_dim, embedding_dim)
+
+    def _separate_heads(self, x: Tensor, num_heads: int) -> Tensor:
+        b, n, c = x.shape
+        x = x.reshape(b, n, num_heads, c // num_heads)
+        return x.transpose(1, 2)  # B x N_heads x N_tokens x C_per_head
+
+    def _recombine_heads(self, x: Tensor) -> Tensor:
+        b, n_heads, n_tokens, c_per_head = x.shape
+        x = x.transpose(1, 2)
+        return x.reshape(b, n_tokens, n_heads * c_per_head)  # B x N_tokens x C
+
+    def preprocess_confidence_map(self, C: Tensor, beta=2)-> Tensor:
+        # Assume C is your confidence map tensor of shape [batch_size, num_heads, seq_len, seq_len]
+        # Z-score normalization
+        mean = C.mean()
+        std = C.std()
+        z_norm_C = (C - mean) / (std + 1e-6)
+
+        # Scale by beta
+        return torch.exp(beta * z_norm_C)
+
+    def forward(self, q: Tensor, k: Tensor, v: Tensor, confidence_map=None) -> Tensor:
+        # Input projections
+        q = self.q_proj(q)
+        k = self.k_proj(k)
+        v = self.v_proj(v)
+
+        # Separate into heads
+        q = self._separate_heads(q, self.num_heads)
+        k = self._separate_heads(k, self.num_heads)
+        v = self._separate_heads(v, self.num_heads)
+
+        # Attention
+        _, _, _, c_per_head = q.shape
+        attn = q @ k.permute(0, 1, 3, 2)  # B x N_heads x N_tokens x N_tokens
+        attn = attn / math.sqrt(c_per_head)
+
+        if self.apply_modification and confidence_map is not None:
+            # Apply the modification with the confidence map
+            confidence_map = self.preprocess_confidence_map(confidence_map)
+            # Flatten the confidence_map to [4096]
+            confidence_map_flat = confidence_map.view(-1)
+            confidence_map_flat = confidence_map_flat.unsqueeze(0).unsqueeze(0).unsqueeze(0)  # Add batch dimension
             attn = attn * confidence_map_flat
 
         attn = torch.softmax(attn, dim=-1)
