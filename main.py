0ed4d30f1f25df44913fdd0664390eb7bbf586a7
damien-feller <148772466+damien-feller@users.noreply.github.com>
2024-03-21 16:10:24 +0000

    uniform background points

--- main.py
+++ main.py
@@ -676,8 +676,11 @@ def sample_points(indices, probabilities, n_points, fg=True, alpha=10.0):
             return []
         # Apply transformation to skew the probabilities
         probabilities_transformed = np.power(probabilities, alpha)
-        print(probabilities_transformed.sum())
-        probabilities_normalized = probabilities_transformed / probabilities_transformed.sum()
+        total_prob = probabilities_transformed.sum()
+        if total_prob > 0:
+            probabilities_normalized = probabilities_transformed / total_prob
+        else:
+            probabilities_normalized = probabilities/probabilities.sum()
         samples = np.random.choice(len(probabilities), size=n_points, replace=False, p=probabilities_normalized)
         return list(zip(indices[1][samples], indices[0][samples]))
     else:
